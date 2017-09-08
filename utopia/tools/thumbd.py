2701ccdd8acc946293725baa7a5602bc3d94decb
David Thorne <david.thorne@manchester.ac.uk>
2017-09-08 15:25:48 +0100

    Updated thumbd client tool

--- utopia/tools/thumbd.py
+++ utopia/tools/thumbd.py
@@ -4,7 +4,7 @@ import urllib2
 
 
 def get_sentiment(category, user, articles):
-    url = 'https://utopia.cs.manchester.ac.uk/thumbd/{0}/query'.format(str(category))
+    url = 'https://utopia.cs.manchester.ac.uk/thumbd/{0}/get'.format(str(category))
     query = {
         'user': str(user),
         'articles': [str(article) for article in articles]
@@ -14,11 +14,22 @@ def get_sentiment(category, user, articles):
 
 
 
-def put_sentiment(category, user, sentiment):
-    url = 'https://utopia.cs.manchester.ac.uk/thumbd/{0}'.format(str(category))
+def set_sentiment(category, user, sentiment):
+    url = 'https://utopia.cs.manchester.ac.uk/thumbd/{0}/set'.format(str(category))
     query = {
         'user': str(user),
         'sentiment': {str(article): value for article, value in sentiment.items()}
+    }
+    data = json.load(urllib2.urlopen(url, data=json.dumps(query), timeout=8))
+    return data
+
+
+
+def unset_sentiment(category, user, articles):
+    url = 'https://utopia.cs.manchester.ac.uk/thumbd/{0}/unset'.format(str(category))
+    query = {
+        'user': str(user),
+        'articles': [str(article) for article in articles]
     }
     data = json.load(urllib2.urlopen(url, data=json.dumps(query), timeout=8))
     return data
